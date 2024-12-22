f7a09a71573556c9dcf7397c310804b8fabd368f
Mustafa <cs.mustafaali@gmail.com>
2024-12-22 10:53:11 -0800

    add proxy

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    credentials: true, // This is important.
+    origin: 'https://mustafafolio.onrender.com/'
+  }),
+);
 
 const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
 const NOW_PLAYING_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
